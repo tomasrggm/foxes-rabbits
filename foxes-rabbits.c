2c984b8fe17f6e5abf9ad37b15b080cd261da52f
TomasRGGM <fierygod9@gmail.com>
2022-03-05 16:06:46 +0000

    global seed and world print

--- foxes-rabbits.c
+++ foxes-rabbits.c
@@ -5,9 +5,14 @@
 
 int M,N;
 char* grid;
+uint32_t seedW; 
 
 bool position_empty(int i, int j){
-    return true;
+    if(grid[M*i + j] == ' '){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 void insert_animal(int i,int j, char atype){
@@ -34,25 +39,47 @@ void generate_element(int n, char atype, uint32_t *seed)
     int i, j, k;
 
     for(k = 0; k < n; k++){
-	i = M * r4_uni(seed);
-	j = N * r4_uni(seed);
-
-    printf("%d com %d\n",i,j); //verificar as coordenadas tocadas
+	i = M * r4_uni(seed);//axis vertical
+	j = N * r4_uni(seed);//axis horizontal
 	if(position_empty(i, j))
 	    insert_animal(i, j, atype);
     }
+}
+
+void worldPrinter(){
+    for(int k = 0; k < M*N; k++){
+        if(k % M == 0){
+            printf("\n");
+        }
+        printf("%c|",grid[k]);
+    }
+    printf("\n");
 }
 
 
 
 //quick example to test: ./foxes-rabbits 22 4 4 3 2 2 1 10 4 123
 int main(int argc, char* argv[]){ //app name[0], gen[1], M[2], N[3], n-rocks[4], n-rabbits[5], rabbit breeding[6], n-foxes[7], fox breeding[8], fox starve[9], seed[10]
-    uint32_t seed = atoi(argv[10]);
+    seedW = atoi(argv[10]);
     M = atoi(argv[2]);
     N = atoi(argv[3]);
     grid = malloc(sizeof(char) * (M*N)); //numero de casas. Vamos pondo uma letra por cada espaco de memoria
-    generate_element(12,'a',&seed);
-    printf("o carater e %c \n",grid[2]); //teste do apontador
     
+    
+    for(int k = 0; k < (M*N); k++){
+        grid[k] = ' ';
+        printf("%d\n",k);
+    }
+
+
+    int nrocks = atoi(argv[4]);
+    int nrabbits = atoi(argv[5]);
+    int nfoxes = atoi(argv[7]);
+    generate_element(nrocks,'*',&seedW);
+    generate_element(nrabbits,'R',&seedW);
+    generate_element(nfoxes,'F',&seedW);
+
+    worldPrinter();
+
     return 1;
 }
