9a03036a2e84011320b8aeb1b2743fe5e17f5d15
Tomas <fierygod9@gmail.com>
2022-03-28 20:00:51 +0100

    openMP update

--- foxes.rabbits.c
+++ foxes.rabbits.c
@@ -10,12 +10,15 @@ int breedingAgeF;
 int starvationAgeF;
 struct entity** grid;
 struct entity** grid_temp;
+//global pointers
+
 uint32_t seedW; 
-int genCheck = 0;
+//--------------------
 // Animal types (or 'names').
 char FOX = 'F';
 char RABBIT = 'R';
 char ROCK = '*';
+//--------------------
 
 
 struct entity{
@@ -27,15 +30,12 @@ struct entity{
 
 
 bool position_empty(int i, int j){
-    return grid[i][j].name == ' ' || grid[i][j].name == 'V' || grid[i][j].name == 'Z';
+    return grid[i][j].name == ' ';
 }
 bool has_rabbit(int i,int j){
     return grid[i][j].name == 'R';
 }
 
-bool is_there(int i, int j, char c){
-    return grid_temp[i][j].name == c && grid[i][j].name != c;
-}
 
 int calculate_movement(int i, int j, int p){
     int c = i * N + j;
@@ -43,37 +43,16 @@ int calculate_movement(int i, int j, int p){
 }
 
 
-void worldPrinter(){
-    for(int k = 0; k < M; k++){
-        for(int j = 0; j < N; j++){
-            if(grid[k][j].name == 'V'){
-                printf("F %d|",grid[k][j].lastGenEat);
-            }else if(grid[k][j].name == 'Z'){
-                printf("R %d|",grid[k][j].age);
-            }else{
-                printf("%c %d|",grid[k][j].name,grid[k][j].age);
-            }
-            
-            
-        }
-        printf("\n");
-    }
-    printf("\n");
-}
-
-
-
-
-
-
 
 void insert_animal(int i,int j, char atype){
     grid[i][j].name = atype;
     grid[i][j].age = 0;
     grid[i][j].lastGenEat = 0;
+    grid[i][j].moved = 0;
     grid_temp[i][j].name = atype;
     grid_temp[i][j].age = 0;
     grid_temp[i][j].lastGenEat = 0;
+    grid_temp[i][j].moved = 0;
 }
 
 
@@ -108,65 +87,68 @@ void generate_element(int n, char atype, uint32_t *seed)
 
 void move_rabbit(int i,int j,int og_i,int og_j){
     int bred = 0;
-    printf("IDADE EH %d\n",grid_temp[og_i][og_j].age);
-    if(grid_temp[og_i][og_j].age == breedingAgeR){
-        printf("ENTROU\n");
-        bred++;
+    
+
+    if(grid_temp[og_i][og_j].age >= breedingAgeR){
         grid_temp[og_i][og_j].age = 0;
+        bred++;
     }
     if(grid_temp[i][j].name == ' '){
-        grid_temp[i][j].name = 'R';
+        grid_temp[i][j].name = RABBIT;
         grid_temp[i][j].age = grid_temp[og_i][og_j].age;
     }
-    else if(is_there(i,j,'R') && grid_temp[og_i][og_j].age > grid_temp[i][j].age){
-        grid_temp[i][j].age = grid_temp[og_i][og_j].age;
-    }
-    else if(is_there(i,j,'F')){
+    else if(grid_temp[i][j].name == FOX){
         grid_temp[i][j].lastGenEat = 0;
     }
-    if(bred == 0){
-        grid_temp[og_i][og_j].age = 0;
-        grid_temp[og_i][og_j].name = ' ';
-        grid_temp[og_i][og_j].lastGenEat = 0;
+    else if(grid_temp[i][j].name == RABBIT && grid_temp[og_i][og_j].age > grid_temp[i][j].age){
+        grid_temp[i][j].age = grid_temp[og_i][og_j].age;
     }
+    grid_temp[og_i][og_j].age = 0;
+    if(bred == 0){
+        grid_temp[og_i][og_j].name = ' ';
+    }
+    
     grid_temp[i][j].moved = 1;
 
 }
 
 void move_fox(int i,int j,int og_i,int og_j){
     int bred = 0;
-    int starve = grid[og_i][og_j].lastGenEat;
-    if(grid_temp[og_i][og_j].age == breedingAgeF){
-        bred++;
+    int starve = grid_temp[og_i][og_j].lastGenEat;
+    
+
+    if(grid_temp[og_i][og_j].age >= breedingAgeF){
         grid_temp[og_i][og_j].age = 0;
         grid_temp[og_i][og_j].lastGenEat = 0;
+        bred++;
     }
+
     if(grid_temp[i][j].name == ' '){
-        grid_temp[i][j].name = 'F';
-        grid_temp[i][j].age = grid[og_i][og_j].age;
+        grid_temp[i][j].name = FOX;
+        grid_temp[i][j].age = grid_temp[og_i][og_j].age;
         grid_temp[i][j].lastGenEat = starve;
     }
-    else if(is_there(i,j,'F')){
-        if(grid_temp[i][j].lastGenEat == 0 && grid_temp[og_i][og_j].age > grid_temp[i][j].age){
+    else if(grid_temp[i][j].name == FOX){
+        if(grid_temp[og_i][og_j].age > grid_temp[i][j].age){
+            
             grid_temp[i][j].age = grid_temp[og_i][og_j].age;
-        }
-        else if(starve > grid_temp[i][j].lastGenEat){
             grid_temp[i][j].lastGenEat = starve;
-            grid_temp[i][j].age = grid_temp[og_i][og_j].age;
         }
-        else if(starve == grid_temp[i][j].lastGenEat && grid_temp[og_i][og_j].age > grid_temp[i][j].age){
-            grid_temp[i][j].age = grid_temp[og_i][og_j].age;
+        else if(grid_temp[i][j].age == grid_temp[og_i][og_j].age && grid_temp[i][j].lastGenEat > starve){
+            grid_temp[i][j].lastGenEat = starve;
         }
-    }else if(grid_temp[i][j].name == 'R'){
-        grid_temp[i][j].name = 'F';
+    }
+    else if(grid_temp[i][j].name == RABBIT){
+        grid_temp[i][j].name = FOX;
         grid_temp[i][j].age = grid_temp[og_i][og_j].age;
         grid_temp[i][j].lastGenEat = 0;
     }
+    grid_temp[og_i][og_j].age = 0;
+    grid_temp[og_i][og_j].lastGenEat = 0;
     if(bred == 0){
-        grid_temp[og_i][og_j].age = 0;
         grid_temp[og_i][og_j].name = ' ';
-        grid_temp[og_i][og_j].lastGenEat = 0;
     }
+
     grid_temp[i][j].moved = 1;
 }
 
@@ -178,10 +160,10 @@ void move_animal (int i, int j, int og_i, int og_j) {
     }
 }
 
-int* get_available_moves(int i, int j){
+int* get_available_moves(int i, int j, int * freeMoves, int* eatMoves){
+    //int* freeMoves = malloc(sizeof(int) * 5);
+    //int* eatMoves = malloc(sizeof(int) * 5);
     bool isFox = grid[i][j].name == FOX;
-    int* freeMoves = malloc(sizeof(int)*5);
-    int* eatMoves = malloc(sizeof(int)*5);
     int movesR = 0;
     int moves = 0;
     for(int k = 0; k < 5;k++){
@@ -192,50 +174,42 @@ int* get_available_moves(int i, int j){
         freeMoves[0] = 1;
         moves++;
     }
+    if(i != 0 && isFox && has_rabbit(i-1,j)){
+        eatMoves[0] = 1;
+        movesR++;
+    }
     if(i != M-1 && position_empty(i+1,j)){
         freeMoves[2] = 1;
         moves++;
+    }
+    if(i != M-1 && isFox && has_rabbit(i+1,j)){
+        eatMoves[2] = 1;
+        movesR++;
     }
     if(j != 0 && position_empty(i,j-1)){
         freeMoves[3] = 1;
         moves++;
     }
+    if(j != 0 && isFox && has_rabbit(i,j-1)){
+        eatMoves[3] = 1;
+        movesR++;
+    }
     if(j != N-1 && position_empty(i,j+1)){
         freeMoves[1] = 1;
         moves++;
     }
-    
-    if(i != 0 && isFox && has_rabbit(i-1,j)){
-        
-        eatMoves[0] = 1;
-        movesR++;
-    }
-    
-    if( i != M-1 && isFox && has_rabbit(i+1,j)){
-        
-        eatMoves[2] = 1;
-        movesR++;
-    }
-    
-    if(j != 0 && isFox && has_rabbit(i,j-1)){
-        
-        eatMoves[3] = 1;
-        movesR++;
-    }
-    
     if(j != N-1 && isFox && has_rabbit(i,j+1)){
-        
         eatMoves[1] = 1;
         movesR++;
     }
     
     if(movesR > 0){
         eatMoves[4] = movesR;
-        free(freeMoves);
+        //free(freeMoves);
         return eatMoves;
     }else{
         freeMoves[4] = moves;
-        free(eatMoves);
+        //free(eatMoves);
         return freeMoves;
     }
 }
@@ -268,94 +242,137 @@ void make_move(int i, int j, int*moves,int c){
     }
 }
 
-void sub_generations(int flag){
+void run_sub_generation(int flag){
     int p = 0;
-    for(int k = 0; k < M; k++){
+    int j;
+    int k;
+    
+    #pragma omp for private(j)
+    for(k = 0; k < M; k+=3){
+        int freeMoves[5] = {0,0,0,0,0};
+        int eatMoves[5] = {0,0,0,0,0};
         p = (k % 2 == flag) ? 0 : 1;
-        for(int j = p; j < N; j += 2){
+
+        for(j = p; j < N; j += 2){
             if((grid[k][j].name == FOX || grid[k][j].name == RABBIT) && grid[k][j].moved == 0){
-                int* moves = get_available_moves(k,j);
-                
+                int* moves = get_available_moves(k,j,freeMoves,eatMoves);
                 if(moves[4] > 0){
-                    
                     int c = calculate_movement(k,j,moves[4]);
-                    
                     make_move(k,j,moves,c);
-                }else{
-                    
-                    if((grid[k][j].age >= breedingAgeF && grid[k][j].name == FOX) || (grid[k][j].age >= breedingAgeR && grid[k][j].name == RABBIT)){
-                        grid[k][j].age--;
-                        grid_temp[k][j].age--;
-                    }
                 }
-                free(moves);
+                //free(moves);
+            }
+        }
+    }
+
+    #pragma omp for private(j)
+    for(k = 1; k < M; k+=3){
+        int freeMoves[5] = {0,0,0,0,0};
+        int eatMoves[5] = {0,0,0,0,0};
+        p = (k % 2 == flag) ? 0 : 1;
+        
+        for(j = p; j < N; j += 2){
+            if((grid[k][j].name == FOX || grid[k][j].name == RABBIT) && grid[k][j].moved == 0){
+                int* moves = get_available_moves(k,j,freeMoves,eatMoves);
+                if(moves[4] > 0){
+                    int c = calculate_movement(k,j,moves[4]);
+                    make_move(k,j,moves,c);
+                }
+                //free(moves);
+            }
+        }
+    }
+
+    #pragma omp for private(j)
+    for(k = 2; k < M; k+=3){
+        int freeMoves[5] = {0,0,0,0,0};
+        int eatMoves[5] = {0,0,0,0,0};
+        p = (k % 2 == flag) ? 0 : 1;
+        
+        for(j = p; j < N; j += 2){
+            if((grid[k][j].name == FOX || grid[k][j].name == RABBIT) && grid[k][j].moved == 0){
+                int* moves = get_available_moves(k,j,freeMoves,eatMoves);
+                if(moves[4] > 0){
+                    int c = calculate_movement(k,j,moves[4]);
+                    make_move(k,j,moves,c);
+                }
+                //free(moves);
             }
         }
     }
 }
 
 void copy_red(){
-    for(int i = 0; i < M; i++){
-        for(int j = 0; j < N; j++){
+    int i,j;
+    #pragma omp for private(j)
+    for(i = 0; i < M; i++){
+        for(j = 0; j < N; j++){
             grid[i][j].name = grid_temp[i][j].name;
-            grid[i][j].lastGenEat = grid_temp[i][j].lastGenEat;
             grid[i][j].age = grid_temp[i][j].age;
+            grid[i][j].lastGenEat = grid_temp[i][j].lastGenEat;
             grid[i][j].moved = grid_temp[i][j].moved;
         }
     }
 }
 
 void copy_black(){
-    for(int i = 0; i < M; i++){
-        for(int j = 0; j < N; j++){
+    int i,j;
+    #pragma omp for private(j)
+    for(i = 0; i < M; i++){
+        for(j = 0; j < N; j++){
             grid[i][j].name = grid_temp[i][j].name;
-            grid[i][j].lastGenEat = grid_temp[i][j].lastGenEat;
             grid[i][j].age = grid_temp[i][j].age;
+            grid[i][j].lastGenEat = grid_temp[i][j].lastGenEat;
             grid[i][j].moved = 0;
             grid_temp[i][j].moved = 0;
-            if(grid[i][j].lastGenEat == starvationAgeF){
+            if(grid[i][j].name == FOX && grid[i][j].lastGenEat == starvationAgeF){
                 grid[i][j].name = ' ';
-                grid[i][j].age = 0;
                 grid[i][j].lastGenEat = 0;
+                grid[i][j].age = 0;
+                grid[i][j].moved = 0;
                 grid_temp[i][j].name = ' ';
-                grid_temp[i][j].age = 0;
                 grid_temp[i][j].lastGenEat = 0;
+                grid_temp[i][j].age = 0;
+                grid_temp[i][j].moved = 0;
             }
         }
     }
 }
 
 
-void generations(int n){
-    for(int i = 0; i < n; i++){
-        printf("GERACAO %d\n TURNO VERMELHO \n",i+1);
-        for(int k = 0; k < M; k++){
-            for(int p = 0; p < N;p++){
-                if(grid[k][p].name == FOX || grid[k][p].name == RABBIT){
-                    grid[k][p].age++;
-                    grid_temp[k][p].age++;
-                    if(grid[k][p].name == FOX){
-                        grid[k][p].lastGenEat++;
-                        grid_temp[k][p].lastGenEat++;
+void run_simulation(int n){
+    int i, j;
+    for(int k = 0; k < n; k++){
+        //printf("%d\n",k);fflush(stdout);
+        #pragma omp parallel 
+        {
+        #pragma omp for private(j)
+        for(i = 0; i < M; i++){
+            for(j = 0; j < N; j++){
+                if(grid[i][j].name == FOX || grid[i][j].name == RABBIT){
+                    grid[i][j].age++;
+                    grid_temp[i][j].age++;
+                    if(grid[i][j].name == FOX){
+                        grid[i][j].lastGenEat++;
+                        grid_temp[i][j].lastGenEat++;
                     }
                 }
             }
-            
+        }
+        
+        run_sub_generation(0);
+
+        copy_red();
+
+        run_sub_generation(1);
+
+        copy_black();
         }
 
-        sub_generations(0);
-        copy_red();
-        worldPrinter();
-        printf("\n TURNO PRETO \n");
-        sub_generations(1);
-        copy_black();
-        worldPrinter();
-        
-        genCheck++;
     }
+    
 }
 
-//quick example to test: ./foxes-rabbits 25 4 4 3 2 1 1 10 4 123
 int main(int argc, char* argv[]){ //app name[0], gen[1], M[2], N[3], n-rocks[4], n-rabbits[5], rabbit breeding[6], n-foxes[7], fox breeding[8], fox starve[9], seed[10]
     
     //get parameters
@@ -373,6 +390,14 @@ int main(int argc, char* argv[]){ //app name[0], gen[1], M[2], N[3], n-rocks[4],
     //start grid
     grid = malloc(sizeof(struct entity*) * M);
     grid_temp = malloc(sizeof(struct entity*) * M);
+
+    //freeMoves = malloc(sizeof(int)*5);
+    //eatMoves = malloc(sizeof(int)*5);
+	
+    /*for(int k = 0; k < 5;k++){
+        freeMoves[k] = 0;
+        eatMoves[k] = 0;
+    }*/
 
     for(int k = 0; k < M; k++){
         grid[k] = malloc(N * sizeof(struct entity));
@@ -392,27 +417,43 @@ int main(int argc, char* argv[]){ //app name[0], gen[1], M[2], N[3], n-rocks[4],
         }
     }
 
-    
-
-
-
-
     generate_element(nrocks,'*',&seedW);
     generate_element(nrabbits,'R',&seedW);
     generate_element(nfoxes,'F',&seedW);
-    worldPrinter();
+
 
     double exec_time;
     exec_time = -omp_get_wtime();
-    generations(ngen);
+    run_simulation(ngen);
     exec_time += omp_get_wtime();
     fprintf(stderr, "%.1fs\n", exec_time);
+
+    int final_rabbits = 0;
+    int final_foxes = 0;
+    int final_rocks = 0;
+
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            if (grid[i][j].name == FOX)
+                final_foxes++;
+            else if (grid[i][j].name == RABBIT)
+                final_rabbits++;
+            else if (grid[i][j].name == ROCK)
+                final_rocks++;
+        }
+    }
+
+
+
+     printf("%d %d %d\n",final_rocks,final_rabbits, final_foxes);
 
 
     for(int i = 0; i < M;i++){
         free(grid[i]);
         free(grid_temp[i]);
     }
+    //free(freeMoves);
+    //free(eatMoves);
     free(grid);
     free(grid_temp);
     return 0;
